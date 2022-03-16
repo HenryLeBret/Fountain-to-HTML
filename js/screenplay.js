873ac0a37d7e2859cef3d8eba8e918ab51873c73
HenryLeBret <jcgroult@gmail.com>
2022-03-16 01:38:51 +0100

    avoid some useless parsing

--- js/screenplay.js
+++ js/screenplay.js
@@ -4,6 +4,12 @@ class Screenplay {
 
         var t = this;
 
+        const navigationKeys = [
+            'Shift', 'Control', 'CapsLock', 'AltGraph',
+            'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown',
+            'PageUp', 'PageDown', 'Home', 'End'
+        ];
+
         t.domId = domId;
         t.parser = parser;
         t.view = display;
@@ -12,13 +18,19 @@ class Screenplay {
 
         // React to user typing Fountain
         t.delayTime;
-        document.getElementById(t.domId).addEventListener('keyup', () => startParsing(), false);    
+        document.getElementById(t.domId).addEventListener('keyup', event => {
+            if (navigationKeys.includes(event.key)
+                || event.isComposing || event.keyCode === 229) {
+                return;
+            }
+            startParsing()
+        }, false);
 
         async function startParsing() {
             clearInterval(t.delayTime); // Only parses one time, if the user does not type after the last parse, no new call will be launched
             t.delayTime = setTimeout(function() {
                 t.view.clearScript();
-                t.parser.parseFountain(); // Wait before parsing, start parsing when the user isn't typing              
+                t.parser.parseFountain(); // Wait before parsing, start parsing when the user isn't typing
             }, t.typeTimeout);
         }
 
